06b774e2ea458aa33cb22e60f047d9b882c56192
Jovana Ristic <jokacoka2012@gmail.com>
2023-08-29 18:41:17 +0200

    promena

--- JovaninKod/drugizadatk.py
+++ JovaninKod/drugizadatk.py
@@ -1,18 +1,32 @@
-import math
-from collections import Counter
+#laksi nacin
 
-lista = ['1','3','5']
+# import math
+# from collections import Counter
 
-def convert_add(a):
-    a1 = [eval(i) for i in a]
-    print(sum(a1))
+# lista = ['1','3','5']
 
-convert_add(lista)    
+# def convert_add(a):
+#     a1 = [eval(i) for i in a]
+#     print(sum(a1))
 
-fruits = ['apple','orange','banana','apple']
-names = ['Yoda','Moses','Joshua','Mark']
+# convert_add(lista)    
 
-#def check_duplicates(a):
-   
+#brzi nacin
+import numpy as np
 
+# lista = np.array(['1','3','5'])
 
+# def convert_add(a):
+#     a1 = a.astype(int)
+#     print(sum(a1))
+
+# convert_add(lista)  
+
+fruits = np.array(['apple','orange','banana','apple'])
+names = np.array(['Yoda','Moses','Joshua','Mark'])
+
+def check_duplicates(a):
+   return a.size != np.unique(a).size
+
+print(check_duplicates(fruits))
+print(check_duplicates(names))

2380025a4dbd47de476755cc78e6b8fd5af6ebec
Nikola Lakic <nlakic@pm.me>
2023-08-29 14:27:28 +0200

    gotov prvi zadatak

--- NikolinKod/prvizadatak.py
+++ NikolinKod/prvizadatak.py
@@ -1,1 +1,8 @@
-print("kurac")
+def divide_or_square():
+    a = float(input("Uneti broj: "))
+    if a % 5 == 0:
+        print( a ** 0.5)
+    else:
+        print(f"Broj {a} nije deljiv sa 5")
+
+divide_or_square()

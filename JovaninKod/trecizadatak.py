113b89486b741bb97763f552e9a4699c9b742802
Jovana Ristic <jokacoka2012@gmail.com>
2023-09-04 16:17:28 +0200

    Zavrsila sam treci dan

--- JovaninKod/trecizadatak.py
+++ JovaninKod/trecizadatak.py
@@ -2,3 +2,13 @@ register = {'Michael':'yes',
             'John':'no',
             'Peter':'yes',
             'Mary':'yes'}
+
+def register_check(r):
+    count = 0
+
+    for name, attendance in r.items():
+        if attendance == "yes":
+            count +=1
+    return count
+
+print(register_check(register))        

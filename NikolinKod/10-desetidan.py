def3cdc5c93d4d3650058a97fc0088647f984d21
Nikola Lakic <nlakic@pm.me>
2023-09-05 17:50:27 +0200

    Deseti dan na pola

---
+++ NikolinKod/10-desetidan.py
@@ -0,0 +1,15 @@
+# def hide_password():
+#
+#     s = str(input('Enter password: '))
+#     lista = ''
+#     for i in s:
+#         lista = lista + "*"
+#     duzina = len(lista)
+#     print(f'Your password is {duzina} characters long')
+#     return lista
+#
+#
+# print(hide_password())
+
+
+list1 = ['1000000', '2356989', '2354672', '9878098']
